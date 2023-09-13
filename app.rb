d1686cace275cb90469b6be9ceaba0419b48f7fb
dani <danifromecuador@gmail.com>
2023-09-13 16:27:51 -0500

    Fix linter errors

--- app.rb
+++ app.rb
@@ -5,26 +5,14 @@ require_relative 'src/genre'
 require_relative 'src/label'
 
 require_relative 'src/source'
-
 require_relative 'src/item'
-
-
-
 require_relative 'src/music_album/music_album'
-
 require_relative 'src/music_album/preserve_music_albums'
-
 require_relative 'src/preserve_genres'
-
 require_relative 'src/preserve_author'
-
 require_relative 'src/game/game'
-
 require_relative 'src/game/preserve_game'
-
 require_relative 'helper'
-
-
 
 class App
   attr_accessor :music_albums
@@ -80,9 +68,9 @@ class App
 
   def list_all_music_albums
     if @music_albums.empty?
-      puts "No albums yet"
+      puts 'No albums yet'
     else
-      puts "Albums:"
+      puts 'Albums:'
       @music_albums.each_with_index do |music_album, index|
         title = music_album.label.title
         artist = music_album.author.first_name
@@ -105,9 +93,9 @@ class App
 
   def list_all_movies
     if @movie.empty?
-      puts "No movies yet"
+      puts 'No movies yet'
     else
-      puts "Movies:"
+      puts 'Movies:'
       @movie.each_with_index do |movie, index|
         title = movie.label.title
         artist = movie.author.first_name
@@ -138,117 +126,68 @@ class App
     publish_date = gets.chomp
 
     if publish_date.match?(/\A\d+\z/)
-
       publish_date = publish_date.to_i
 
     else
-
       puts "ERROR: Invalid answer. Value set to 'Unkown'"
-
       publish_date = 0
-
     end
-
-
 
     print 'Can be Archived (Y/N): '
-
     silet = gets.chomp.downcase
 
-
-
     if silet == 'y'
-
       silet = true
-
     elsif silet == 'n'
-
       silet = false
-
     else
-
       puts "ERROR: Invalid answer. Value set to 'N'"
-
       silet = false
-
     end
 
-
-
     @movie.push(Movie.new(@genre, @author, @source, @label, publish_date, silet))
-
     puts 'Movie added successfully!'
   end
 
   def add_a_game
     add_genre
-
     add_author('game')
-
     add_source
-
     add_label
-
-
-
     print 'Publish Date (YEAR): '
-
     publish_date = gets.chomp
 
-
-
     if publish_date.match?(/\A\d+\z/)
-
       publish_date = publish_date.to_i
-
     else
-
       puts "ERROR: Invalid answer. Value set to 'Unkown'"
-
       publish_date = 0
-
     end
 
-
-
     print 'Is it multiplayer (Y/N): '
-
     multiplayer = gets.chomp.downcase
 
-
-
     if multiplayer == 'y'
-
       multiplayer = true
 
     elsif multiplayer == 'n'
-
       multiplayer = false
-
     else
-
       puts "ERROR: Invalid answer. Value set to 'N'"
-
       multiplayer = false
-
     end
 
-
     print 'When was the last time played: '
-
     last_played_at = gets.chomp.downcase
-
-
     @games.push(Game.new(@genre, @author, @source, @label, publish_date, multiplayer, last_played_at))
-
     puts 'Game added successfully!'
   end
 
   def list_all_games
     if @games.empty?
-      puts "No games yet"
+      puts 'No games yet'
     else
-      puts "Games:"
+      puts 'Games:'
       @games.each_with_index do |game, index|
         title = game.label.title
         author = "#{game.author.first_name[0].capitalize}. #{game.author.last_name}"
@@ -270,129 +209,76 @@ class App
 
   def list_all_sources
     if @movie.empty?
-
-      puts "No movie sources yet"
-
+      puts 'No movie sources yet'
     else
-
-      puts "Movie Sources:"
-
-
-
+      puts 'Movie Sources:'
       movie_sources = @movie.map { |movie| movie.source.source_name }
-
       unique_movie_sources = movie_sources.uniq
-
       unique_movie_sources.each_with_index do |source_name, index|
         puts "#{index + 1}: #{source_name}"
       end
-
     end
   end
 
   def add_a_music_album
     add_genre
-
     add_author('music_album')
-
     add_source
-
     add_label
-
     print 'Publish Date (YEAR): '
-
     publish_date = gets.chomp
-
-
-
     if publish_date.match?(/\A\d+\z/)
-
       publish_date = publish_date.to_i
-
     else
-
       puts "ERROR: Invalid answer. Value set to 'Unkown'"
-
       publish_date = 0
-
     end
-
-
 
     print 'Can be Archived (Y/N): '
-
     on_spotify = gets.chomp.downcase
 
-
-
     if on_spotify == 'y'
-
       on_spotify = true
-
     elsif on_spotify == 'n'
-
       on_spotify = false
-
     else
-
       puts "ERROR: Invalid answer. Value set to 'N'"
-
       on_spotify = false
-
     end
 
-
-
     @music_albums.push(MusicAlbum.new(@genre, @author, @source, @label, publish_date, on_spotify))
-
     puts 'Music Album added successfully!'
   end
 
   def list_all_genres
     if @genres.empty?
-
-      puts "No genres yet"
-
+      puts 'No genres yet'
     else
-
-      puts "Genres:"
-
+      puts 'Genres:'
       @genres.each_with_index do |genre, index|
         puts "#{index + 1}: #{genre.genre_name}"
       end
-
     end
   end
 
   def list_all_authors
     if @authors.empty?
-
-      puts "No authors yet"
-
+      puts 'No authors yet'
     else
-
-      puts "Authors:"
-
+      puts 'Authors:'
       @authors.each_with_index do |author, index|
         puts "#{index + 1}: #{author.first_name} #{author.last_name}"
       end
-
     end
   end
 
   def quit
     PreserveMusicAlbums.new.save_music_albums(@music_albums)
-
     PreserveGenres.new.save_genres(@genres)
-
     PreserveAuthor.new.save_authors(@authors)
-
     PreserveMovies.new.save_movies(@movie)
-
     PreserveGames.new.save_games(@games)
-
     puts 'Thank you for using this app!'
-
     exit
   end
 end
