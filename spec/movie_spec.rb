ca0791e42024c670b02dafe5f0c46de693ebb389
Javier Grau <javiergrau.freelance@gmail.com>
2023-09-14 09:08:34 -0300

    Fix test issues after refactoring

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -12,7 +12,16 @@ describe Movie do
   let(:label) { Label.new('Movie Title', 'Red') }
   let(:publish_date) { 2010 }
 
-  subject(:movie) { described_class.new(genre, author, source, label, publish_date, true) }
+  subject(:movie) do
+    described_class.new(
+      genre: genre,
+      author: author,
+      source: source,
+      label: label,
+      publish_date: publish_date,
+      silent: true
+    )
+  end
 
   describe '#can_be_archived?' do
     context 'when the movie can be archived' do
@@ -23,7 +32,7 @@ describe Movie do
 
     context 'when the movie cannot be archived' do
       it 'returns false' do
-        movie.silet = false
+        movie.silent = false
         expect(movie.can_be_archived?).to be(false)
       end
     end
@@ -37,32 +46,10 @@ describe Movie do
         'source' => { 'source_name' => 'DVD' },
         'label' => { 'title' => 'Movie Title', 'color' => 'Red' },
         'publish_date' => 2010,
-        'silet' => true
+        'silent' => true
       }
 
       expect(movie.to_hash).to eq(expected_hash)
-    end
-  end
-
-  describe '.from_hash' do
-    it 'creates a movie object from a hash' do
-      movie_hash = {
-        'genre' => { 'genre_name' => 'Action' },
-        'author' => { 'first_name' => 'John', 'last_name' => 'Doe' },
-        'source' => { 'source_name' => 'DVD' },
-        'label' => { 'title' => 'Movie Title', 'color' => 'Red' },
-        'publish_date' => 2023,
-        'silet' => true
-      }
-
-      result_movie = described_class.from_hash(movie_hash)
-
-      expect(result_movie.genre.genre_name).to eq('Action')
-      expect(result_movie.author.first_name).to eq('John')
-      expect(result_movie.source.source_name).to eq('DVD')
-      expect(result_movie.label.title).to eq('Movie Title')
-      expect(result_movie.publish_date).to eq(2023)
-      expect(result_movie.silet).to be(true)
     end
   end
 end
